730510442abd0cf67aecea1573d16fac45407451
Ude897053 <alancumbio@gmail.com>
2022-07-12 22:19:09 -0300

    cambios

--- ficha_personal.php
+++ ficha_personal.php
@@ -1,5 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//con la linea 2,3,4 reporta errores en php
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+$nombre = "Alan Denis Garcia";
+$edad = 26;
+$aPeliculas = array("La caida del Halcon negro", "Que paso ayer", "terminator")
+
+?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +19,44 @@
     <title>ficha-personal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
+
 <body>
-    <main>
-        <div class="container"> 
-            <div class="row">
-                <div class="col-12 text-center py-5">
-                    <h1>ficha personal</h1>
-                </div>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Ficha Personal</h1>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <tr>
+                        <th>Fecha</th>
+                        <td><?php echo date("d/m/Y"); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Nombre y Apellido</th>
+                        <td><?php echo $nombre; ?></td>
+                    </tr>
+                    <tr>
+                        <th>edad</th>
+                        <td><?php echo $edad; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Peliculas Favoritas</th>
+                        <td><?php echo $aPeliculas[0]; ?><br>
+                            <?php echo $aPeliculas[1]; ?> <br>
+                            <?php echo $aPeliculas [2]; ?>
+                         
+                        </td>
+                    </tr>
+            </div>
+        </div>
+        </div>
+        </table>
     </main>
-    
-    
+
+
 </body>
+
 </html>
