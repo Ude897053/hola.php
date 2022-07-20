a3a53d5dca2e4862fb79a69e2361f861204094b7
Ude897053 <alancumbio@gmail.com>
2022-07-20 00:04:14 -0300

    cambios

--- numeros_pares.php
+++ numeros_pares.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-for (si=0; si <= 100; si+=2) {
+for ($i=0; $i <= 100; $i+=2) {
 
     echo si . "<br>";
 }
