aef9aadefcd2d2b0d6dc96234400cca190a12304
Ude897053 <alancumbio@gmail.com>
2022-07-26 22:43:30 -0300

    cambios

--- neto.php
+++ neto.php
@@ -4,9 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 function calcularNeto ($bruto){
-
+return $bruto - ($bruto * 0.17);
 
 }
+echo "El sueldo neto es $" . calcularNeto(80000);
 
 
 
